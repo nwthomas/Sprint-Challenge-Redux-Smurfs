464dfaf6de052ec293a11a829552e6161b755758
Nathan Thomas <nwthomas@me.com>
2019-02-01 09:31:14 -0800

    Create update/delete action creators in actions/index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -6,6 +6,12 @@ export const GET_SMURFS_ERROR = "GET_SMURFS_ERROR";
 export const POST_SMURF_START = "POST_SMURF_START";
 export const POST_SMURF_SUCCESS = "POST_SMURF_SUCCESS";
 export const POST_SMURF_ERROR = "POST_SMURF_ERROR";
+export const DELETE_SMURF_START = "DELETE_SMURF_START";
+export const DELETE_SMURF_SUCCESS = "DELETE_SMURF_SUCCESS";
+export const DELETE_SMURF_ERROR = "DELETE_SMURF_ERROR";
+export const UPDATE_SMURF_START = "UPDATE_SMURF_START";
+export const UPDATE_SMURF_SUCCESS = "UPDATE_SMURF_SUCCESS";
+export const UPDATE_SMURF_ERROR = "UPDATE_SMURF_ERROR";
 
 export const getSmurfs = _ => dispatch => {
   dispatch({ type: GET_SMURFS_START });
@@ -31,13 +37,26 @@ export const postSmurf = smurf => dispatch => {
     });
 };
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const deleteSmurf = id => dispatch => {
+  dispatch({ type: DELETE_SMURF_START });
+  axios
+    .post(`http://localhost:3333/smurfs/${id}`)
+    .then(res => {
+      dispatch({ type: DELETE_SMURF_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: DELETE_SMURF_ERROR, payload: err.data });
+    });
+};
+
+export const updateSmurf = smurf => dispatch => {
+  dispatch({ type: UPDATE_SMURF_START });
+  axios
+    .post(`http://localhost:3333/smurfs/${smurf.id}`)
+    .then(res => {
+      dispatch({ type: UPDATE_SMURF_START, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: UPDATE_SMURF_ERROR, payload: err.data });
+    });
+};
